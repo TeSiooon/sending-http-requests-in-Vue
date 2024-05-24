68b041a185646268d3535cb6d039979392018a09
TeSiooon <piotr.kowalczyk9908@gmail.com>
2024-05-24 12:01:52 +0200

    getting data

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,31 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://react-kurs-771f0-default-rtdb.firebaseio.com/surveys.json')
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const res = [];
+          for (const id in data) {
+            res.push({ id: id, name: data[id].name, rating: data[id].rating });
+          }
+          this.results = res;
+        });
+    },
   },
 };
 </script>
